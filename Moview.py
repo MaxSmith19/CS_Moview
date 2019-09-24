d64f9de76c98d072d59539ea8d5388716be66a13
Max Smith <MS42220@suffolkone.ac.uk>
2019-09-24 11:59:25 +0100

    CSS changes to hello.html

--- Moview.py
+++ Moview.py
@@ -1,9 +1,8 @@
 import tweepy, mysql.connector, re, math
-"""
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') 
 def main():
    return render_template('hello.html')
 
@@ -12,8 +11,6 @@ def handle_data():
     if request.method == "POST":
         projectpath=request.form["projectFilePath"]
     return render_template("displayMovie.html",movieName=projectpath)
-
-
     
 if __name__ == '__main__':
    app.run(debug = True)
@@ -32,6 +29,7 @@ def oAuth():
     mining(api)
 
 def appendTable(movieChoice):
+    pass
     mydb= mysql.connector.connect(
         host="databases.suffolkone.ac.uk",
         user="MS42220",
@@ -77,51 +75,60 @@ def mining(api):
     row_count = mycursor.rowcount
     movieRating = 0
     ratingList=[]
-    movieAverage=0
+    movieAverage=1
     count=100
     movieP=0
     movieN=0
-    if row_count ==0:
-        for tweet in tweepy.Cursor(api.search,q=movieChoice+"-filter:retweets",count=count,lang="en",wait_on_rate_limit=True,tweet_mode="extended").items(count):
-            newTweet= removeEmoji(tweet.full_text) 
-            FinalTweet=CharOnly(newTweet) #removes unnecessary emojis and unreadable items from the string, to make it easier to analyse.           
-            splitTweet=FinalTweet.split()
-            with open(r"Dictionary\\positiveWords.txt","r") as positiveDict:
-                pLine=positiveDict.readlines() #CHANGE THIS TO USE THE WEIGHTED FILE
-            with open(r"Dictionary\\negativeWords.txt","r") as negativeDict:
-                nLine=negativeDict.readlines()
-            for word in splitTweet: #for each word in tweet
-                for pw in pLine: #for each positive word in the positive file
-                    word=word.lower().strip() #lowercase
-                    pw=pw.lower().strip()#lowercase
-                    if pw in word:
-                        movieRating=movieRating+1
-                        break
-                for iw in nLine:
-                    word=word.lower().strip()
-                    iw=iw.lower().strip()
-                    if iw in word:
-                        movieRating=movieRating-1
-                        break
-            if movieRating>0:
-                ratingList.append("Positive")
-            if movieRating<=0:
-                ratingList.append("Negative")
-        movieP = ratingList.count("Positive")
-        movieN = ratingList.count("Negative")
-        movieAverage=movieP/movieN*100
-        print(str(movieAverage))
-        #currently it is only determined if it's good or bad, not how good or bad it is.
-    else:
-        print("This Movie already exists within the database.")
-        appendTable()
+    movieTotal=0
+    try:
+        if row_count ==0:
+            for tweet in tweepy.Cursor(api.search,q=movieChoice+"-filter:retweets",count=count,lang="en",wait_on_rate_limit=True,tweet_mode="extended").items(count):
+                newTweet= removeEmoji(tweet.full_text) 
+                FinalTweet=CharOnly(newTweet) #removes unnecessary emojis and unreadable items from the string, to make it easier to analyse.           
+                splitTweet=FinalTweet.split()
+                with open(r"Dictionary\\positiveWords.txt","r") as positiveDict:
+                    pLine=positiveDict.readlines() #CHANGE THIS TO USE THE WEIGHTED FILE
+                with open(r"Dictionary\\negativeWords.txt","r") as negativeDict:
+                    nLine=negativeDict.readlines()
+                for word in splitTweet: #for each word in tweet
+                    for pw in pLine: #for each positive word in the positive file
+                        word=word.lower().strip() #lowercase
+                        pw=pw.lower().strip()#lowercase
+                        if pw in word:
+                            movieRating=movieRating+3
+                            break
+                    for iw in nLine:
+                        word=word.lower().strip()
+                        iw=iw.lower().strip()
+                        if iw in word:
+                            movieRating=movieRating-1
+                            break
+                    movieTotal=movieTotal+movieRating
+            #     if movieRating>0:
+            #         ratingList.append("Positive")
+            #     if movieRating<=0:
+            #         ratingList.append("Negative")
+            # movieP = ratingList.count("Positive")
+            # movieN = ratingList.count("Negative")
+            # movieAverage=movieP/movieN 
+            print(movieTotal/100)
+            print(movieAverage)
+        else:
+            print("This Movie already exists within the database.")
+            appendTable(movieChoice)
+    except ZeroDivisionError:
+        movieAverage+1
+    finally:
+        appendTable(movieChoice)
+    #currently it is only determined if it's good or bad, not how good or bad it is.
+
 #Stack overflow  - removes emojis from text, changing them to ascii instead.
 def removeEmoji(inputString):
     return inputString.encode('ascii', 'ignore').decode('ascii')
 def CharOnly(inputString):
     #Also sourced from Stack Overflow
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", inputString).split()) 
-
+"""
 oAuth()
 # Movie TABLE CONSISTS OF: MovieID, name
 # tweets TABLE CONSISTS OF: TweetID, TweetText, MovieID - Removed, due to lack of necessity, as i can analyse them as i get them.
